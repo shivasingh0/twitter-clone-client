568dab2a66bf17abec685a8b89abe676a9de68cd
shivasingh0 <shiva2003rajawat@gmail.com>
2024-07-30 19:06:18 +0530

    first commit

---
+++ src/models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, "Please provide username"],
+        unique: [true, "This username not available"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: [true, "Try another email"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"]
+    },
+    isVarified: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date
+})
